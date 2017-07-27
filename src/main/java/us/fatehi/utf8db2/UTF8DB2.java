587461d87da381dfb4385996a023474bbc081e88
Sualeh Fatehi <sualeh@hotmail.com>
2017-07-26 22:11:29 -0400

    Working example

--- src/main/java/us/fatehi/utf8db2/UTF8DB2.java
+++ src/main/java/us/fatehi/utf8db2/UTF8DB2.java
@@ -4,7 +4,9 @@ package us.fatehi.utf8db2;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class UTF8DB2
@@ -14,15 +16,38 @@ public class UTF8DB2
   {
     try (final Connection connection = getConnection())
     {
-      final String insertTableSQL = "INSERT INTO TAB1 (COL1, COL2) VALUES (?, ?)";
-      try (PreparedStatement preparedStatement = connection
+      // 1. Clear table for this test
+      try (final Statement statement = connection.createStatement();)
+      {
+        statement.execute("DROP TABLE TAB1");
+        statement.execute("CREATE TABLE TAB1 (COL1 VARCHAR(3))");
+      }
+      System.out.println("Test table dropped and re-created");
+
+      // 2. Insert bad UTF-8 bytes
+      final String insertTableSQL = "INSERT INTO TAB1 (COL1) VALUES (?)";
+      try (final PreparedStatement preparedStatement = connection
         .prepareStatement(insertTableSQL);)
       {
-
-        preparedStatement.setString(1, "tst");
-        preparedStatement.setString(2, "sys");
-
+        preparedStatement
+          .setBytes(1, new byte[] { (byte) 0xE2, (byte) 0x82, (byte) 0xAC });
         preparedStatement.executeUpdate();
+
+        preparedStatement
+          .setBytes(1, new byte[] { (byte) 0xBF, (byte) 0x20, (byte) 0x52 });
+        preparedStatement.executeUpdate();
+      }
+      System.out.println("Data inserted into table");
+
+      // 3. Read them back out
+      // http://www-01.ibm.com/support/docview.wss?uid=swg21684365
+      try (final ResultSet results = connection.createStatement()
+        .executeQuery("SELECT * FROM TAB1");)
+      {
+        while (results.next())
+        {
+          System.out.println(results.getString(1));
+        }
       }
     }
   }
@@ -33,6 +58,8 @@ public class UTF8DB2
     final Properties connectionProps = new Properties();
     connectionProps.put("user", "db2inst1");
     connectionProps.put("password", "sualeh-db2-password");
+
+    System.setProperty("db2.jcc.charsetDecoderEncoder", "3");
 
     final Connection conn = DriverManager
       .getConnection("jdbc:db2://192.168.56.101:50000/SAMPLE", connectionProps);
