419bbbac6fcc075666b7dfe63346abef868b644d
Sualeh Fatehi <sualeh@hotmail.com>
2017-07-27 11:12:32 -0400

    Allowing for different connection strings

--- src/main/java/us/fatehi/utf8db2/UTF8DB2.java
+++ src/main/java/us/fatehi/utf8db2/UTF8DB2.java
@@ -14,7 +14,20 @@ public class UTF8DB2
   public static void main(final String[] args)
     throws Exception
   {
-    try (final Connection connection = getConnection())
+
+    /*
+     * final String url = "jdbc:db2://192.168.56.101:50000/SAMPLE";
+     * final String username = "db2inst1"; final String password =
+     * "sualeh-db2-password";
+     */
+    final String url = "jdbc:db2://sfatehi-vm1:50000/bevap:securityMechanism=4;currentSchema=SFATEHI;";
+    final String username = "sfatehi";
+    final String password = null;
+
+    try (final Connection connection = getConnection(url,
+                                                     username,
+                                                     password,
+                                                     true))
     {
       // 1. Clear table for this test
       try (final Statement statement = connection.createStatement();)
@@ -29,12 +42,20 @@ public class UTF8DB2
       try (final PreparedStatement preparedStatement = connection
         .prepareStatement(insertTableSQL);)
       {
-        preparedStatement
-          .setBytes(1, new byte[] { (byte) 0xE2, (byte) 0x82, (byte) 0xAC });
+        preparedStatement.setBytes(1,
+                                   new byte[] {
+                                                (byte) 0xE2,
+                                                (byte) 0x82,
+                                                (byte) 0xAC
+                                   });
         preparedStatement.executeUpdate();
 
-        preparedStatement
-          .setBytes(1, new byte[] { (byte) 0xBF, (byte) 0x20, (byte) 0x52 });
+        preparedStatement.setBytes(1,
+                                   new byte[] {
+                                                (byte) 0xBF,
+                                                (byte) 0x20,
+                                                (byte) 0x52
+                                   });
         preparedStatement.executeUpdate();
       }
       System.out.println("Data inserted into table");
@@ -52,17 +73,25 @@ public class UTF8DB2
     }
   }
 
-  private static Connection getConnection()
+  private static Connection getConnection(final String url,
+                                          final String username,
+                                          final String password,
+                                          final boolean handleBrokenUF8)
     throws SQLException
   {
     final Properties connectionProps = new Properties();
-    connectionProps.put("user", "db2inst1");
-    connectionProps.put("password", "sualeh-db2-password");
+    connectionProps.put("user", username);
+    if (password != null)
+    {
+      connectionProps.put("password", password);
+    }
 
-    System.setProperty("db2.jcc.charsetDecoderEncoder", "3");
+    if (handleBrokenUF8)
+    {
+      System.setProperty("db2.jcc.charsetDecoderEncoder", "3");
+    }
 
-    final Connection conn = DriverManager
-      .getConnection("jdbc:db2://192.168.56.101:50000/SAMPLE", connectionProps);
+    final Connection conn = DriverManager.getConnection(url, connectionProps);
 
     System.out.println("Connected to database, " + conn.toString());
     return conn;
