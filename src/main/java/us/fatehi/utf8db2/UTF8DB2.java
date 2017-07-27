a885e02509b6b3a73a23a40633e4d057ebf8c64f
Sualeh Fatehi <sualeh@hotmail.com>
2017-07-26 21:30:05 -0400

    Insert

--- src/main/java/us/fatehi/utf8db2/UTF8DB2.java
+++ src/main/java/us/fatehi/utf8db2/UTF8DB2.java
@@ -3,6 +3,7 @@ package us.fatehi.utf8db2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -13,7 +14,16 @@ public class UTF8DB2
   {
     try (final Connection connection = getConnection())
     {
+      final String insertTableSQL = "INSERT INTO TAB1 (COL1, COL2) VALUES (?, ?)";
+      try (PreparedStatement preparedStatement = connection
+        .prepareStatement(insertTableSQL);)
+      {
 
+        preparedStatement.setString(1, "tst");
+        preparedStatement.setString(2, "sys");
+
+        preparedStatement.executeUpdate();
+      }
     }
   }
 
